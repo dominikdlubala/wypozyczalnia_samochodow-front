c3d43394ff4968e47a89fbb84c3b93f47111db66
Damian Matysiak <d.matysiak@adres.pl>
2024-11-20 21:51:00 +0100

    admin panel stylized

--- clientapp/src/pages/AdminPage.tsx
+++ clientapp/src/pages/AdminPage.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { getAllCars } from "../services/CarService";
-import { Car } from "../types";
 
 const AdminPage = () => {
   const [cars, setCars] = useState<any[]>([]);
@@ -19,40 +18,46 @@ const AdminPage = () => {
   }, []);
 
   return (
-    <div className="text-center">
-      <h1>Admin Panel</h1>
-      <p>Witamy w panelu administracyjnym!</p>
-      {error && <p style={{ color: "red" }}>{error}</p>}
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Marka</th>
-            <th>Model</th>
-            <th>Typ paliwa</th>
-            <th>Pojemność silnika</th>
-            <th>Typ nadwozia</th>
-            <th>Kolor</th>
-            <th>Cena za dzień</th>
-            <th>Rok produkcji</th>
-          </tr>
-        </thead>
-        <tbody>
-          {cars.map((car) => (
-            <tr key={car.id}>
-              <td>{car.id}</td>
-              <td>{car.brand}</td>
-              <td>{car.model}</td>
-              <td>{car.fuelType}</td>
-              <td>{car.capacity} L</td>
-              <td>{car.bodyType}</td>
-              <td>{car.color}</td>
-              <td>{car.pricePerDay} PLN</td>
-              <td>{car.productionYear}</td>
+    <div className="page page-admin">
+      <div className="content-wrapper-admin">
+        <h1>Admin Panel</h1>
+        <h2 className="mt-4">Samochody</h2>
+        {error && <p style={{ color: "red" }}>{error}</p>}
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Marka</th>
+              <th>Model</th>
+              <th>Typ paliwa</th>
+              <th>Pojemność silnika</th>
+              <th>Typ nadwozia</th>
+              <th>Kolor</th>
+              <th>Cena za dzień</th>
+              <th>Rok produkcji</th>
+              <th>Edytuj</th>
+              <th>Usuń</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {cars.map((car) => (
+              <tr key={car.id}>
+                <td>{car.id}</td>
+                <td>{car.brand}</td>
+                <td>{car.model}</td>
+                <td>{car.fuelType}</td>
+                <td>{car.capacity} L</td>
+                <td>{car.bodyType}</td>
+                <td>{car.color}</td>
+                <td>{car.pricePerDay} PLN</td>
+                <td>{car.productionYear}</td>
+                <td><button className="btn-action-edit-car">Edytuj</button></td>
+                <td><button className="btn-action-delete-car">Usuń</button></td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
