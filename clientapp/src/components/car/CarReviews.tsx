151687e495a55f10d454231219205c1a967db669
KrystianOchmanski <krysochmanski1337@gmail.com>
2024-12-17 11:19:20 +0100

    review look change

--- clientapp/src/components/car/CarReviews.tsx
+++ clientapp/src/components/car/CarReviews.tsx
@@ -1,27 +1,29 @@
 import { Review } from "../../types";
 
 interface CarReviewProps {
-    reviewsData?: Review[] | null; 
+  reviewsData?: Review[] | null;
 }
 
 export default function CarReviews({ reviewsData }: CarReviewProps) {
-
-    return (
-        <div className="car-reviews">
-            {
-                reviewsData
-                &&
-                reviewsData.map(review => (
-                    <div className="review" key={review.id}>
-                        <div className="review-header">
-                            <div className="review-user">{review.userId}</div>
-                            <div className="review-data">{review.dateOfIssue?.toLocaleString()}</div>
-                        </div>
-                        <div className="review-stars">Gwiazdki: {review.starsOutOfFive}</div>
-                        <div className="review-content">{review.reviewContent}</div>
-                    </div>
-                ))
-            }
-        </div>
-    )
+  return (
+    <div className="car-reviews">
+      {reviewsData &&
+        reviewsData.map((review) => (
+          <div className="review" key={review.id}>
+            <div className="review-header">
+              <div className="review-user">{review.username}</div>
+              <div className="review-data">
+                {review.dateOfIssue
+                  ? new Date(review.dateOfIssue).toLocaleDateString()
+                  : ""}
+              </div>
+            </div>
+            <div className="review-stars">
+              Gwiazdki: {review.starsOutOfFive}
+            </div>
+            <div className="review-content">{review.reviewContent}</div>
+          </div>
+        ))}
+    </div>
+  );
 }
